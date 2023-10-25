63665d14bfe7fe220f2cdcbe00c8a192709ce22b
yaasiinaxmed <yaskassoy@gmail.com>
2023-10-25 18:24:14 +0300

    create transaction model

---
+++ api/models/transactions.model.js
@@ -0,0 +1,52 @@
+import mongoose from 'mongoose'
+
+const transactionSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: String,
+        required: true
+    },
+    bedrooms: {
+        type: Number,
+        required: true
+    },
+    bathrooms: {
+        type: Number,
+        required: true
+    },
+    location: {
+        type: String,
+        required: true
+    },
+    propertyType: {
+        type: String,
+        required: true
+    },
+    type: {
+        type: String,
+        required: true
+    },
+    userId: {
+        type: String,
+        required: true
+    },
+    senderId: {
+        type: String,
+        required: true
+    },
+    requestId: {
+        type: String,
+        required: true
+    }
+}, {timestamps: true})
+
+const transactionModel = mongoose.model("transaction", transactionSchema)
+
+export default transactionModel
