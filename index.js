dd00e0547d8a176a66492c42c9fa4de1f3c05d8d
yaasiinaxmed <yaskassoy@gmail.com>
2023-10-26 19:22:25 +0300

    create public folder & home page Server

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
+import express from 'express'
 import server from "./api/server.js";
 
 const port = "3000"
+
+server.use(express.static('public'));
+
+server.get('/', (req, res) => {
+  res.sendFile('index.html');
+});
 
 server.listen(port, () => console.log(`Server is running at ${port}`))
