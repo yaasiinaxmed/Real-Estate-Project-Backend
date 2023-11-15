98c8c9ed3acd33e762687f76dceb2cabdfed561c
yaasiinaxmed <yaskassoy@gmail.com>
2023-11-15 14:40:43 +0300

    added cors

--- api/server.js
+++ api/server.js
@@ -3,11 +3,13 @@ import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 import userRouter from './routes/user.route.js'
 import propertiesRouter from './routes/properties.route.js'
+import cors from "cors"
 
 const server = express()
 
 dotenv.config()
 server.use(express.json())
+server.use(cors())
 
 // connect mongoDB
 mongoose.connect(process.env.DATABASE_URL).then(() => {
